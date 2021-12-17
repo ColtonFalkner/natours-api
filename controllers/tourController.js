6bec618b217a6fbea5f2a3100aa2e2913878686e
Colton <coltonfalkner@gmail.com>
2021-12-17 13:18:51 -0500

    CRUD operations completed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -78,9 +78,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.deleteOne({ _id: req.params.id });
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
